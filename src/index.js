71b10e2d440b6a79699c02ec623988dfd42e8189
Gabriel Rangel <grangelda@icloud.com>
2021-07-17 15:27:30 -0300

    checkout form impl; buttons handlers impl;

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import {EventCard} from "./components/EventCard.js";
 
+function slipViewToggle () {
+    const layer1 = document.querySelector('.layer1')
+    const display = layer1.style.display === 'none'
+    layer1.style.display = !display ? 'none' : 'block'
+}
+
+function handleCheckout (e) {
+    console.log('checkout')
+    e.preventDefault()
+}
+
+function handleBetButtonClick (e) {
+    const isButton = e.target.tagName === 'BUTTON'
+    const button = isButton ? e.target : e.target.parentNode
+
+    const isClicked = button.classList.contains('green')
+    isClicked ? button.classList.remove('green') : button.classList.add('green')
+}
+
+document.querySelectorAll('.slip-view-toggle').forEach(toggle => {
+    toggle.addEventListener('click', slipViewToggle)
+})
+
+document.querySelector('#checkout').addEventListener('submit', handleCheckout)
+
 fetch('https://www.mocky.io/v2/59f08692310000b4130e9f71')
     .then((res) => res.json())
     .then((data) => {
@@ -7,5 +32,9 @@ fetch('https://www.mocky.io/v2/59f08692310000b4130e9f71')
 
         data.forEach((event) => {
             event.markets.length > 0 && main.appendChild(EventCard(event))
+        })
+
+        document.querySelectorAll('.event-card button').forEach(betButton => {
+            betButton.addEventListener('click', handleBetButtonClick)
         })
     })
